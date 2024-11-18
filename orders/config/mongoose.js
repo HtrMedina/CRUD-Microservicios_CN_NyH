64f10fa6f36a0472f46584e6ec0e96820be9ecb3
HtrMedina <h.medinaalcaraz@ugto.mx>
2024-11-18 11:17:08 -0600

    Añadir archivo carpeta de ordenes

---
+++ orders/config/mongoose.js
@@ -0,0 +1,18 @@
+// Importar mongoose para interactuar con MongoDB
+const mongoose = require('mongoose')
+
+// Función para conectar a la base de datos de MongoDB de manera asincrónica
+const connectDB = async () => {
+    try {
+      // Intentamos conectarnos a MongoDB usando mongoose con la URI de conexión
+      const db = await mongoose.connect("mongodb://localhost/ordenes");
+      // Si la conexión es exitosa, mostramos un mensaje con el nombre del host de la conexión
+      console.log(`Conectado a Mongo db ${mongoose.connection.host}`);
+    } catch (error) {
+      // Si ocurre un error durante la conexión, lo capturamos y mostramos un mensaje de error
+      console.log(`MongoDB Error ${error}`);
+    }
+  };
+
+  // Exportamos la función connectDB para poder utilizarla en otros archivos
+  module.exports = connectDB;
